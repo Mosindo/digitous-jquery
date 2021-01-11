c19fa59ab3c46415c6675b68989746270464c34e
Mosindo <64804043+Mosindo@users.noreply.github.com>
2021-01-11 17:05:42 +0100

    exo 15 fini

---
+++ exercice15/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    function text(){
+        $("p #text").addClass("class").css("float","right");
+        };
+
+        $(".btn").click(function(){
+        text();
+        })
+})
