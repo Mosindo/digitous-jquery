0e77fc42b16247dcb409f65c35c83a9efacddb43
Mosindo <64804043+Mosindo@users.noreply.github.com>
2021-01-11 23:15:32 +0100

    exo 29 fini

---
+++ exercice29/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $(document).ready(function () {
+        $(".btn").click(function () {
+            if($('input').val().length <5){
+                $('input').addClass("is-invalid")
+                $('input[type=text]').val("5 caratères requis ");
+            }
+            else {
+               // $('input[type=text].sitebg').val('000000');
+               
+            }
+        })
+    })
+
+})
