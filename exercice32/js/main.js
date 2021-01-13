f20dc5a97e18ed341415d609b2ca5af170a31808
Mosindo <64804043+Mosindo@users.noreply.github.com>
2021-01-13 15:48:04 +0100

    experience exo 32

--- exercice32/js/main.js
+++ exercice32/js/main.js
@@ -3,8 +3,9 @@ $(document).ready(function () {
         $.ajax({
             url: 'https://restcountries.eu/rest/v2/name/france',
             success: function (data) {
-                console.log('Nom :', data[0].name);
-                console.log('Capital :', data[0].capital);
+                //console.log('Nom :', data[0].name);
+                //console.log('Capital :', data[0].capital);
+                $("#exercise").html(`<p>la capital de la ${data[0].name} est ${data[0].capital}</p>`)
             }
         });
     })
