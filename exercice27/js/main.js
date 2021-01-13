de7e38c6c6627d870fba09e78504684a34396c75
Mosindo <64804043+Mosindo@users.noreply.github.com>
2021-01-13 11:15:29 +0100

    exo 32 fini

--- exercice27/js/main.js
+++ exercice27/js/main.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
-    $(".btn").click(function () {
-        $("input").addClass("is-valid");
-      
-})
-})
+    //rendre l'input invalide
+       $("button").click(function(){
+         $("input").addClass("is-valid")
+        })   
+    //changer les lettres tapés en étoile au fur et à mesure
+        $("input").keyup(function(){
+        var len=$("input").val().length
+        var chaine=''
+        for (var i = 0; i < len; i++) {
+        chaine=chaine+'*';
+        $("input").val(chaine)
+       }
+    })   
+  });
